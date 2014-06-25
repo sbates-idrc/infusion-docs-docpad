c48c6c26b31557ad15350500beecfb8dfb9635d4
Simon Bates <simon@bitstructures.com>
2014-06-25 16:55:59 -0400

    Add "Edit on GitHub"

--- docpad.js
+++ docpad.js
@@ -1,12 +1,19 @@
 var rewriteUrls = function (content) {
-    return content.replace(/(<a\s[^>]*href="[\w-/\.]+)\.md(["#])/gm, '$1.html$2');
+    return content.replace(/(<a\s[^>]*href="[\w-/\.]+)\.md(["#])/gm, "$1.html$2");
 };
+
+var githubLocation = function () {
+    // in case we're on Windows, replace "\" in the path with "/"
+    var relativePath = this.document.relativePath.replace(/\\/g, "/");
+    return "https://github.com/fluid-project/infusion-docs/blob/master/" + relativePath;
+}
 
 module.exports = {
     renderSingleExtensions: true,
     plugins: {
         handlebars: {
             helpers: {
+                githubLocation: githubLocation,
                 rewriteUrls: rewriteUrls
             }
         }
